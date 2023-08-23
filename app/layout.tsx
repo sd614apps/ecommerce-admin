5a3d5c081de9e4085273bb4ccb860d1517613e07
SD614 Apps <sd614.apps@gmail.com>
2023-08-24 00:25:05 +0530

    Updated store modal and root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import { ModalProvider } from '@/providers/modal-provider'
+import { ToastProvider } from '@/providers/toast-provider'
 
 import './globals.css'
 
@@ -18,10 +19,11 @@ export default function RootLayout({
 }: {
     children: React.ReactNode
 }) {
-    return (
+        return (
         <ClerkProvider>
         <html lang="en">
             <body className={inter.className}>
+                <ToastProvider />
                 <ModalProvider />
                 {children}
             </body>
